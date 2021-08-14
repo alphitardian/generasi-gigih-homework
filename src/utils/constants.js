73596ae0b7ddfbbc500118c80db0586a1c3eafef
Ardian Alphita <ardianpramudya81@gmail.com>
2021-08-14 13:41:34 +0700

    Add deployment url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-undef */
 export const SPOTIFY_ENDPOINT = "https://api.spotify.com/v1";
 export const API_SCOPE = "playlist-modify-private";
-export const REDIRECT_URI = "http://localhost:3000/";
+// https://spotify-gigih.vercel.app/
+// http://localhost:3000/
+export const REDIRECT_URI = "https://spotify-gigih.vercel.app/";
 export const SIGNIN_URL = `https://accounts.spotify.com/en/authorize?client_id=${
   process.env.REACT_APP_SPOTIFY_CLIENT_ID
 }&response_type=${"token"}&redirect_uri=${REDIRECT_URI}&scope=${API_SCOPE}`;
