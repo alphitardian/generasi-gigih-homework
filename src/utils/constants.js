fc2631715016aa84bd20bdf9de6c4d5bde6f92ce
Ardian Alphita <ardianpramudya81@gmail.com>
2021-08-19 17:58:35 +0700

    Add new playlist scope

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,7 @@
 /* eslint-disable no-undef */
 export const SPOTIFY_ENDPOINT = "https://api.spotify.com/v1";
 export const API_SCOPE =
-  "playlist-modify-private user-library-read user-read-email user-read-private";
-// https://spotify-gigih.vercel.app/
-// http://localhost:3000/
+  "playlist-modify-private user-library-read user-read-email user-read-private playlist-read-private playlist-read-collaborative";
 export const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URL;
 export const SIGNIN_URL = `https://accounts.spotify.com/en/authorize?client_id=${
   process.env.REACT_APP_SPOTIFY_CLIENT_ID
